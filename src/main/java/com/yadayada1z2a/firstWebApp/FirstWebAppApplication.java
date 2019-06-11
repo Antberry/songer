ea97c99ecf4b585ef5f992dfa8486a43e8f7abd2
Antberry <aniej2013@gmail.com>
2019-06-11 16:07:54 -0700

    new repo

---
+++ src/main/java/com/yadayada1z2a/firstWebApp/FirstWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.yadayada1z2a.firstWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebAppApplication.class, args);
+	}
+
+}
