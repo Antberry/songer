5e3b2a86608da75adae7fb6d54db6975d95aca59
Antberry <aniej2013@gmail.com>
2019-06-12 12:58:15 -0700

    submitting something so i can finish it later

--- src/main/java/com/yadayada1z2a/firstWebApp/FirstWebAppApplication.java
+++ src/main/java/com/yadayada1z2a/firstWebApp/FirstWebAppApplication.java
@@ -2,9 +2,28 @@ package com.yadayada1z2a.firstWebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class FirstWebAppApplication {
+
+	@Controller
+	public class HelloWorldController {
+		@RequestMapping("/hello")
+		public String hello(){
+			return "Hello World";
+		}
+
+		@RequestMapping("/capitalize/{text}")
+		public String upperCaseHello(@PathVariable String text){ ;
+			return text.toUpperCase();
+		}
+
+
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstWebAppApplication.class, args);
